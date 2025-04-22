c9920dc3968d98398727f74fc6acfc0e31d1f426
areeshafaisal44 <areesha.faisal02@gmail.com>
2025-04-23 00:04:24 +0500

    CV updated

--- components/hero.tsx
+++ components/hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
             className="flex flex-col sm:flex-row gap-4 justify-center"
           >
             <Button size="lg" variant="default" className="rounded-none" asChild>
-              <Link href="/AreeshaFaisalResume.pdf" target="_blank" rel="noopener noreferrer">
+              <Link href="/AreeshaFaisalUpdatedCV.pdf" target="_blank" rel="noopener noreferrer">
                 View Resume
               </Link>
             </Button>
